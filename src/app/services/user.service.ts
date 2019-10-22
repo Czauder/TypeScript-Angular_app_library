399de3fa8d7319e822fd5d97f58b6580d7e73139
pczauderma <pczauderna@prescientco.com>
2019-10-22 15:52:41 +0200

    added method buyBookAsGift

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+
+import { Transaction } from '../models/transaction-model';
 import { User } from '../models/user-model';
+import { BookstoreService } from './bookstore.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +10,21 @@ import { User } from '../models/user-model';
 export class UserService {
   private bankAccount = 1500;
 
-  constructor() { }
+  constructor(private bookstoreService: BookstoreService) {}
 
   public addMoneytoWallet(user: User): void {
     user.wallet.currentCash += this.bankAccount;
     console.log('dodano środki z Banku');
+  }
+
+  public buyBookAsGift(
+    userBuying: User,
+    userReceiving: User,
+    transaction: Transaction
+  ): void {
+    if (userBuying.wallet.currentCash >= transaction.book.price) {
+      userReceiving.boughtBooks.push(transaction.book);
+      userBuying.wallet.currentCash -= transaction.book.price;
+    }
   }
 }
