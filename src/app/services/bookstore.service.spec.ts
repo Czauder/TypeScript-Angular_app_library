c9c8905e849742c502121b9f57603da38a196693
pczauderma <pczauderna@prescientco.com>
2019-10-24 20:46:16 +0200

    added test in spectator

--- src/app/services/bookstore.service.spec.ts
+++ src/app/services/bookstore.service.spec.ts
@@ -1,9 +1,52 @@
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
 import { BookstoreService } from './bookstore.service';
+import { User } from '../models/user-model';
+import { Wallet } from '../models/wallet-model';
+import { Book } from '../models/book-model';
+import { bookType } from '../book-type.enum';
+import { Price } from '../models/price-model';
+import { Author } from '../models/author-model';
+import { CurrencyType } from '../currency-type.enum';
+import { Transaction } from '../models/transaction-model';
+import { transactionType } from '../transaction-type.enum';
 
 describe('BookstoreService', () => {
   let spectator: SpectatorService<BookstoreService>;
-  const createService = createServiceFactory(BookstoreService);
+  const createService = createServiceFactory({
+    service: BookstoreService
+  });
 
   beforeEach(() => (spectator = createService()));
+
+  it('should be created', () => {
+    expect(spectator.service).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    const userMock = new User('Jaś Kot', 'kot@gmail.com', new Wallet(1555));
+
+    const bookMock = new Book(
+      'Jakaś Książka',
+      bookType.Comedy,
+      new Author('Pan', 'Ktoś'),
+      new Price(150, CurrencyType.euro),
+      new Price(50, CurrencyType.euro)
+    );
+
+    const transaction = new Transaction(
+      userMock,
+      transactionType.borrowBooks,
+      new Date(),
+      bookMock
+    );
+
+    spectator.service.borrowBook(transaction);
+    const expectBorrowBook = spectator.service.books.find(
+      x => x === transaction.book
+      );
+      
+    console.log(expectBorrowBook);
+    console.log(transaction.user.borrowBooks);
+    expect(spectator.service.books.includes(expectBorrowBook)).toBeFalsy();
+  });
 });
--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,9 +1,27 @@
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+
 import { UserService } from './user.service';
+import { User } from '../models/user-model';
+import { Wallet } from '../models/wallet-model';
 
 describe('UserService', () => {
   let spectator: SpectatorService<UserService>;
-  const createService = createServiceFactory(UserService);
+  const createService = createServiceFactory({
+    service: UserService
+  });
 
   beforeEach(() => (spectator = createService()));
+
+  it('should be created', () => {
+    expect(spectator.service).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    const cashMock = 15;
+    const userMock = new User('Jaś Kot', 'kot@gmail.com', new Wallet(150));
+    const expectUserWallet = userMock.wallet.balance + cashMock;
+
+    spectator.service.addMoneyToWallet(userMock, cashMock);
+    expect(userMock.wallet.balance).toEqual(expectUserWallet);
+  });
 });
