eabfd5910c0ba5044cc88455181ae9ab88a52a43
pczauderma <pczauderna@prescientco.com>
2019-10-24 22:36:57 +0200

    added test in spectator

--- src/app/services/bookstore.service.spec.ts
+++ src/app/services/bookstore.service.spec.ts
@@ -23,9 +23,9 @@ describe('BookstoreService', () => {
   });
 
   it('should be created', () => {
-    const userMock = new User('Jaś Kot', 'kot@gmail.com', new Wallet(1555));
+    const user = new User('Jaś Kot', 'kot@gmail.com', new Wallet(1555));
 
-    const bookMock = new Book(
+    const book = new Book(
       'Jakaś Książka',
       bookType.Comedy,
       new Author('Pan', 'Ktoś'),
@@ -34,19 +34,58 @@ describe('BookstoreService', () => {
     );
 
     const transaction = new Transaction(
-      userMock,
+      user,
       transactionType.borrowBooks,
       new Date(),
-      bookMock
+      book
+    );
+  });
+
+  it('should borrow book', () => {
+    const user = new User('Jaś Kot', 'kot@gmail.com', new Wallet(1555));
+
+    const book = new Book(
+      'Jakaś Książka',
+      bookType.Comedy,
+      new Author('Pan', 'Ktoś'),
+      new Price(150, CurrencyType.euro),
+      new Price(50, CurrencyType.euro)
+    );
+    const transaction = new Transaction(
+      user,
+      transactionType.borrowBooks,
+      new Date(),
+      book
     );
 
     spectator.service.borrowBook(transaction);
     const expectBorrowBook = spectator.service.books.find(
       x => x === transaction.book
-      );
-      
-    console.log(expectBorrowBook);
-    console.log(transaction.user.borrowBooks);
+    );
     expect(spectator.service.books.includes(expectBorrowBook)).toBeFalsy();
   });
+
+  it('should add book', () => {
+    const bookMock = new Book(
+      'Jakaś Książka',
+      bookType.Comedy,
+      new Author('Pan', 'Ktoś'),
+      new Price(150, CurrencyType.euro),
+      new Price(50, CurrencyType.euro)
+    );
+
+    spectator.service.addBook(bookMock);
+    expect(spectator.service.books.length).toBeGreaterThan(0);
+  });
+
+  it('should add user', () => {
+    const user = new User('Jaś Kot', 'kot@gmail.com', new Wallet(1555));
+
+    spectator.service.addUser(user);
+    expect(spectator.service.clients.includes(user)).toBeTruthy();
+  });
+
+
+//  czy legnth w tablicy jest taki sam jak wcześniej, przy chęci kupna książki ale braku pieniędzy. 
+// do zmiennej obecną ilość książek i porównać. 
 });
